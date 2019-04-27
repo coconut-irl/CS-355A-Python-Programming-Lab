d259c0d934bb1b2c1a3ea5973304a7b8c98bceef
coconut-irl <coconutx206@gmail.com>
2019-04-27 17:21:05 +0530

    Initial commit

---
+++ Mid Semester Exam/section_a.py
@@ -0,0 +1,21 @@
+# Question 1-4
+
+my_value=20
+print(my_value)
+
+# Question 1-5
+
+x=1
+y=4
+not(x-y>0 or (2!=2))
+
+
+# Question 1-7
+
+p="computer science"
+print(p[-10:-4])
+
+# Question 1-8
+
+for i in range(2,4):
+    print(i)
