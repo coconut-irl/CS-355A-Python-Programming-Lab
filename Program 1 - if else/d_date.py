a20ae73fd3186feda1d63ccd0d166f5cd2c4b432
coconut-irl <coconutx206@gmail.com>
2019-04-27 09:09:04 +0530

    Formatting

--- Program 1 - if else/d_date.py
+++ Program 1 - if else/d_date.py
@@ -1,9 +1,12 @@
 # This program checks if a given date is valid and increments the date if it is valid
+
 date=input("Enter the date: ")
+
 dd,mm,yy=date.split('/')
 dd=int(dd)
 mm=int(mm)
 yy=int(yy)
+
 if(mm==1 or mm==3 or mm==5 or mm==7 or mm==8 or mm==10 or mm==12):
     max1=31
 elif(mm==4 or mm==6 or mm==9 or mm==11):
@@ -12,6 +15,7 @@ elif(yy%4==0 and yy%100!=0 or yy%400==0):
     max1=29
 else:
     max1=28
+
 if(mm<1 or mm>12):
     print("Date is invalid.")
 elif(dd<1 or dd>max1):
