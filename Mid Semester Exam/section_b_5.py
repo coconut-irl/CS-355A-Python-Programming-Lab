782ba90407ecd403770252c7d465818912ff8fe9
coconut-irl <coconutx206@gmail.com>
2019-04-28 08:50:19 +0530

    Initial commit

---
+++ Mid Semester Exam/section_b_5.py
@@ -0,0 +1,20 @@
+# Question 5-c-1
+
+total=0
+def add(arg1,arg2):
+    total=arg1+arg2
+    return total
+add(20,30)
+print(total)
+
+# Question 5-c-2
+
+s1=set("my name is Mohan and Mohan is my name".split())
+print(s1)
+
+# Question 5-c-3
+
+tupleA=(11,42,78,69,45,32,65,89)
+for i in tupleA:
+    if(i%2!=0):
+        print(i) 
